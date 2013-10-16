5e6b53894cfe47844b9b03467c2dd357aa0a3f47
ursusursus <v.brecka@gmail.com>
2013-10-16 03:59:03 +0200

    tretia uloha

--- uloha3.c
+++ uloha3.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <mpi.h>
 
 void printMatrix(int mRows, int mColumns, int m[mRows][mColumns]) {
 	int row, column;
@@ -23,10 +24,10 @@ void populateMatrix(int mRows, int mColumns, int m[mRows][mColumns]) {
 void multiplyMatrices(int aRows, int aColumns, int aM[aRows][aColumns], int bRows, int bColumns, int bM[bRows][bColumns], int rRows, int rColumns, int rM[rRows][rColumns]) {
 	int i, j, k;
 	for(i = 0; i < aRows; i++){
-	    for(j = 0; j < aRows; j++){
+	    for(j = 0; j < bColumns; j++){
 	    	// Pre-init with zero
 	    	rM[i][j] = 0;
-	        for(k = 0; k < aRows - 1; k++){
+	        for(k = 0; k < aColumns; k++){
 	            rM[i][j] += aM[i][k] * bM[k][j];
 	        }
 	    }
@@ -37,52 +38,92 @@ void multiplyMatrices(int aRows, int aColumns, int aM[aRows][aColumns], int bRow
 * Nasobenie matic
 */
 int main(int argc, char *argv[]) {
-	// Init
+	int size, rank;
+
 	const int aRows = 3;
-	const int aColumns = 2;
-	// Kktina ta inicializacia
-	// int matrixA[3][2] = {{8,1}, {8,6}, {9,1}};
+	const int aColumns = 3;
 	int matrixA[aRows][aColumns];
 
-	// Init
-	const int bRows = 2;
+	const int bRows = 3;
 	const int bColumns = 3;
-	// Kktina ta inicializacia
-	// int matrixB[2][3] = {{1,5,2}, {3,0,5}};
 	int matrixB[bRows][bColumns];
 
+	MPI_Init(&argc, &argv);
+
+	MPI_Comm_size(MPI_COMM_WORLD, &size);
+	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
 	if(aColumns != bRows) {
-		printf("Pocet stlpcov prvej matice sa musi rovnat poctu riadkov druhej matice!");
+		printf("Pocet stlpcov prvej matice sa musi rovnat poctu riadkov druhej matice!\n");
 		return 0;
 	}
 
-	// Populate matrices
-	srand(time(NULL));
-	populateMatrix(aRows, aColumns, matrixA);
-	populateMatrix(bRows, bColumns, matrixB);
+	if(size != aRows) {
+		printf("Pocet procesov musi byt rovny poctu riadkov prvej matice!\n");
+	}
 
-	printf("Matrix A\n");
-	printMatrix(aRows, aColumns, matrixA);
+	if(rank == 0) {
+		srand(time(NULL));
 
-	printf("Matrix B\n");
-	printMatrix(bRows, bColumns, matrixB);
+		// Populate matrices
+		populateMatrix(aRows, aColumns, matrixA);
+		populateMatrix(bRows, bColumns, matrixB);
 
+		printf("Matrix A\n");
+		printMatrix(aRows, aColumns, matrixA);
 
-	// Init
-	const int rRows = aRows;
-	const int rColumns = bColumns;
-	// Kktina ta inicializacia
-	// int matrixResult[3][2] = {0};
-	int matrixResult[rRows][rColumns];
+		printf("Matrix B\n");
+		printMatrix(bRows, bColumns, matrixB);
 
-	multiplyMatrices(
-		aRows, aColumns, matrixA,
-		bRows, bColumns, matrixB,
-		rRows, rColumns, matrixResult
+	}
+
+	const int smRows = 1;
+	const int smColumns = aColumns;
+	int sMatrix[smRows][smColumns];
+
+	// Scatter matrix A by rows
+	// so everyone gets one row
+	MPI_Scatter(
+		matrixA, smColumns, MPI_INT,
+		sMatrix, smColumns, MPI_INT,
+		0, MPI_COMM_WORLD
 		);
 
-	printf("Multiplied Matrix\n");
-	printMatrix(rRows, rColumns, matrixResult);
+	// Broadcast matrix B to everyone
+	MPI_Bcast(matrixB, bRows * bColumns, MPI_INT, 0, MPI_COMM_WORLD);
+
+	const int mRows = smRows;
+	const int mColumns = bColumns;
+	int mMatrix[mRows][mColumns];
+
+	// Multiply submatrix with matrix B
+	multiplyMatrices(
+		smRows, smColumns, sMatrix,
+		bRows, bColumns, matrixB,
+		mRows, mColumns, mMatrix
+		);
+
+	const int rRows = aRows;
+	const int rColumns = bColumns;
+	int resultMatrix[rRows][rColumns];
+
+	// Pozor, tu count je pocet prijatych
+	// elementov na proces, nie vsetkych spolu
+	// Gather multiplied rows from everyone
+	// aj join them into final multiplied matrix
+	MPI_Gather(
+		mMatrix, mRows * mColumns, MPI_INT,
+		resultMatrix, mRows * mColumns, MPI_INT,
+		0, MPI_COMM_WORLD
+		);
+
+	// Root prints the result
+	if(rank == 0) {
+		printf("Multiplied Matrix\n");
+		printMatrix(rRows, rColumns, resultMatrix);
+	}
+
+	MPI_Finalize();
 
 	return 0;
 }
